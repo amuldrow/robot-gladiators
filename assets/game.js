230830eb35bf93837f9d080ad919a1181649345e
A DIONN <ashley.muldrow@yahoo.com>
2021-11-16 08:29:30 -0500

    bugfixplayername

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,6 @@
 /* GAME FUNCTIONS */
 
+
 // function to generate a random numeric value
 var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
@@ -79,6 +80,7 @@ var startGame = function() {
   if (playerInfo.health > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    
 
     // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyObj = enemyInfo[i];
@@ -169,10 +171,18 @@ switch (shopOptionPrompt) {
 /* END GAME FUNCTIONS */
 
 /* GAME INFORMATION / VARIABLES */
-
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+ while (name=== ""|| name === null) {
+   name = prompt("What is your robot's name?");
+ }
+ console.log("Your robot's name is " + name);
+  return name;
+};
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
